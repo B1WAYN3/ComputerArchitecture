cda6331fa512a0001fe2be3fbd53f17827fa808e
B1WAYNE <e.sarceno@wustl.edu>
2024-10-31 13:46:27 -0500

    value gets printed but test bench fails

--- Assignments/HomeWork1/top.sv
+++ Assignments/HomeWork1/top.sv
@@ -312,7 +312,7 @@ module extend(input  logic [31:7] instr,
                // J-type (jal)
       3'b011:   immext = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
                // U-TYpe 
-      3'b11:    immext = { instr[31:12] , 12'b0 };
+      3'b111:    immext = { instr[31:12] , 12'b0 };
       default: immext = 32'bx; // undefined
     endcase
 endmodule
